baf203cdcd9a5a6c1a31a7d9bd5c2164b4ea8ec5
josepht96 <Joe.Thomas@allscripts.com>
2021-04-24 15:15:16 -0400

    updating url

--- Go/goproject/foundation.go
+++ Go/goproject/foundation.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	controllers.RegisterControllers()
-	fmt.Println("Listening at: localhost:5000")
+	fmt.Println("Listening at: http://localhost:5000")
 	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
 		log.Fatal(err)
