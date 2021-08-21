fe29ced4ff40962d9bc02140601dffd42741aba8
Joe Thomas <josephbeasleythomas@gmail.com>
2021-08-21 10:33:14 -0400

    changes

--- data_structures/main.go
+++ data_structures/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 //array +
 //linked list +
 //stack +
@@ -11,9 +13,28 @@ package main
 //heaps
 //self balancing tree
 
+type test struct {
+}
+
 func main() {
-	//linkList()
+	//pointer()
+	linkList()
 	//doubleLinkList()
 	//stackStart()
-	queueStart()
+	//queueStart()
+}
+
+func pointer() {
+	num := 5
+	fmt.Println(&num)
+	var p1 *int = &num
+	fmt.Println(p1)
+	fmt.Println(*p1)
+	fmt.Println(&p1)
+	var p2 *int = &num
+	fmt.Println(p2)
+	fmt.Println(*p2)
+	fmt.Println(&p2)
+	*p2 = 6
+	fmt.Println(num)
 }
--- go/serviceMonitor/go.mod
+++ go/serviceMonitor/go.mod
@@ -1,3 +1,5 @@
 module github.com/josepht96/learning/go/serviceMonitor
 
 go 1.14
+
+require github.com/gorilla/websocket v1.4.2 // indirect
