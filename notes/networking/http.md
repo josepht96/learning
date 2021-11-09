474cc1620de573ebe5f92ea823bbde3689483492
Thomas, Joe <josephbeasleythomas@gmail.com>
2021-11-08 22:14:41 -0500

    websocket

--- notes/networking/http.md
+++ notes/networking/http.md
@@ -5,3 +5,11 @@ Web. It is defined in [RFC 1945] and [RFC 2616]. HTTP is implemented in two prog
 program and a server program. The client program and server program, executing on different end
 systems, talk to each other by exchanging HTTP messages. HTTP defines the structure of these
 messages and how the client and server exchange the messages. 
+
+HTTP uses TCP as its underlying transport protocol (rather than running on top of UDP). The HTTP
+client first initiates a TCP connection with the server. Once the connection is established, the browser
+and the server processes access TCP through their socket interfaces. As described in Section 2.1, on
+the client side the socket interface is the door between the client process and the TCP connection; on
+the server side it is the door between the server process and the TCP connection. The client sends
+HTTP request messages into its socket interface and receives HTTP response messages from its
+socket interface. Similarly, the HTTP server receives request messages
---
+++ projects/websockets/server/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+    "log"
+    "net/http"
+    "time"
+ 
+    "github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func socketHandler(w http.ResponseWriter, r *http.Request) {
+    // Upgrade our raw HTTP connection to a websocket based one
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Print("Error during connection upgrade:", err)
+        return
+    }
+    defer conn.Close()
+ 
+    // The event loop
+    for {
+        message := getTime()
+		messageType := websocket.TextMessage
+        if err != nil {
+            log.Println("Error during message reading:", err)
+            break
+        }
+        log.Printf("Received: %s", message)
+        err = conn.WriteMessage(messageType, message)
+        if err != nil {
+            log.Println("Error during message writing:", err)
+            break
+        }
+    }
+}
+ 
+func getTime() ([]byte) {
+	t := time.Now().Format("2006-01-02 15:04:05.000000000")
+	return []byte(fmt.Sprintf("time: %s\n", t))
+	//w.Write([]byte(tString))
+}
+ 
+func main() {
+    http.HandleFunc("/socket", socketHandler)
+	fmt.Println("server starting at http://localhost:8080/socket")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
