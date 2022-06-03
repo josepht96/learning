27ba299333e55a491ca4104589c717e72ede062f
josepht96 <joebthomas4@gmail.com>
2022-06-02 19:06:56 -0500

    lc

---
+++ dsa/lc/graph/graph_logic.go
@@ -0,0 +1,37 @@
+Dijkstras algorithm over the US
+Every state capital is a node
+Every edge is a straight path to adjacent states capital
+calculate the fastest route to the node from the node of choice
+update each node you check
+store the shortest distance from start node to that visited node
+by adding shortest distance from current node
+if the current nodes route to the visited node is less than its current
+shortestDistance (starts -1 or something 
+to indicate it hasnt been visited) then replace shortestDistance
+Keep track of visited nodes. After checking each adjacent node,
+go to next node unless its been visited
+type WeightedGraph struct {
+	StatesMap map([string]State)
+	Routes []Route
+}
+
+type State struct {
+	StateName string
+	ShortestPath
+	PreviousState
+	Routes []*Route
+}
+
+type Route struct {
+	CityA string
+	CityB string
+	Distance int
+}
+
+Build WeightedGraph
+Add State
+Add Routes
+For each route, add pointer to route to CityA and
+CityB Routes array in StatesMap
+
+For each key in StatesMap
