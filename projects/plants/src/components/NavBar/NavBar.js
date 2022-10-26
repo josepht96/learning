222715c0e7246bea61cf9726cc81bb328058a857
josepht96 <joebthomas4@gmail.com>
2022-10-26 09:56:17 -0500

    adding

--- projects/plants/src/components/NavBar/NavBar.js
+++ projects/plants/src/components/NavBar/NavBar.js
@@ -4,9 +4,19 @@ function NavBar() {
     return (
         <div className="NavBar">
             <div className="NavBar-body">
-                <p>
-                    Plants
-                </p>
+            <div className='NavBar-title-box'>
+                    <div className='NavBar-title'>
+                        Plants and stuff
+                    </div>
+                </div>
+                <div className='NavBar-links-box'>
+                    <div className='NavBar-link'>
+                        Home
+                    </div>
+                    <div className='NavBar-link'>
+                        About
+                    </div>
+                </div>
             </div>
         </div>
     );
