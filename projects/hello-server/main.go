d12a08d41444ec527ef53313194c2efccc3c00e8
josepht96 <joebthomas4@gmail.com>
2022-12-05 09:18:10 -0600

    add

--- projects/hello-server/main.go
+++ projects/hello-server/main.go
@@ -1,15 +1,41 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
+
+type Response struct {
+	Status     string  `json:"status"`
+	StatusCode int     `json:"statusCode"`
+	Body       Message `json:"body"`
+}
+
+type Message struct {
+	Message string `json:"message"`
+	Time    string `json:"time"`
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		response := "hello from test-server"
-		w.Write([]byte(response))
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		dt := time.Now()
+		data := Response{
+			Status:     "OK",
+			StatusCode: 200,
+			Body: Message{
+				Message: "hello from test-server",
+				Time:    dt.String(),
+			},
+		}
+		fmt.Println(data)
+		json.NewEncoder(w).Encode(data)
 	})
-	log.Printf("server is listening at %s", "localhost:8080\n")
+	log.Printf("server is listening at %s", "http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
