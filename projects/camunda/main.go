ffab52c1252bd8314d8ad5796bf3c39ac01a2588
josepht96 <joebthomas4@gmail.com>
2023-02-15 12:22:05 -0600

    add

--- projects/camunda/main.go
+++ projects/camunda/main.go
@@ -1,12 +1,142 @@
 package main
 
 import (
-	"net/http"
+	"context"
+	"fmt"
+	"log"
+	"os"
 
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/camunda/zeebe/clients/go/v8/pkg/entities"
+	"github.com/camunda/zeebe/clients/go/v8/pkg/worker"
+	"github.com/camunda/zeebe/clients/go/v8/pkg/zbc"
 )
 
+const ZeebeAddr = "0.0.0.0:26500"
+
+var readyClose = make(chan struct{})
+
+/*
+Sample application that connects to a cluster on Camunda Cloud, or a locally deployed cluster.
+
+When connecting to a cluster in Camunda Cloud, this application assumes that the following
+environment variables are set:
+
+ZEEBE_ADDRESS
+ZEEBE_CLIENT_ID
+ZEEBE_CLIENT_SECRET
+ZEEBE_AUTHORIZATION_SERVER_URL
+
+Hint: When you create client credentials in Camunda Cloud you have the option
+to download a file with the lines above filled out for you.
+
+When connecting to a local cluster or node, this application assumes default port and no
+authentication or encryption enabled.
+*/
 func main() {
-	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":2112", nil)
+	gatewayAddr := os.Getenv("ZEEBE_ADDRESS")
+	var plainText bool
+
+	if gatewayAddr == "" {
+		gatewayAddr = ZeebeAddr
+		plainText = true
+	}
+	credsProvider, err := zbc.NewOAuthCredentialsProvider(&zbc.OAuthProviderConfig{
+		ClientID:               "test-auth",
+		ClientSecret:           "d6apt9pQkI6666bC17YiH3Jjp8mBEy6w",
+		Audience:               ZeebeAddr,
+		AuthorizationServerURL: "http://localhost:30000/realms/master/protocol/openid-connect/token",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	zbClient, err := zbc.NewClient(&zbc.ClientConfig{
+		GatewayAddress:         gatewayAddr,
+		UsePlaintextConnection: plainText,
+		CredentialsProvider:    credsProvider,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	// deploy workflow
+	ctx := context.Background()
+	response, err := zbClient.NewDeployResourceCommand().AddResourceFile("order-process-4.bpmn").Send(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(response.String())
+
+	// create a new workflow instance
+	variables := make(map[string]interface{})
+	variables["orderId"] = "31243"
+
+	request, err := zbClient.NewCreateInstanceCommand().BPMNProcessId("order-process-4").LatestVersion().VariablesFromMap(variables)
+	if err != nil {
+		panic(err)
+	}
+
+	result, err := request.Send(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result.String())
+
+	jobWorker := zbClient.NewJobWorker().JobType("payment-service").Handler(handleJob).Open()
+
+	<-readyClose
+	jobWorker.Close()
+	jobWorker.AwaitClose()
+}
+
+func handleJob(client worker.JobClient, job entities.Job) {
+	jobKey := job.GetKey()
+
+	headers, err := job.GetCustomHeadersAsMap()
+	if err != nil {
+		// failed to handle job as we require the custom job headers
+		failJob(client, job)
+		return
+	}
+
+	variables, err := job.GetVariablesAsMap()
+	if err != nil {
+		// failed to handle job as we require the variables
+		failJob(client, job)
+		return
+	}
+
+	variables["totalPrice"] = 46.50
+	request, err := client.NewCompleteJobCommand().JobKey(jobKey).VariablesFromMap(variables)
+	if err != nil {
+		// failed to set the updated variables
+		failJob(client, job)
+		return
+	}
+
+	log.Println("Complete job", jobKey, "of type", job.Type)
+	log.Println("Processing order:", variables["orderId"])
+	log.Println("Collect money using payment method:", headers["method"])
+
+	ctx := context.Background()
+	_, err = request.Send(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Successfully completed job")
+	close(readyClose)
+}
+
+func failJob(client worker.JobClient, job entities.Job) {
+	log.Println("Failed to complete job", job.GetKey())
+
+	ctx := context.Background()
+	_, err := client.NewFailJobCommand().JobKey(job.GetKey()).Retries(job.Retries - 1).Send(ctx)
+	if err != nil {
+		panic(err)
+	}
 }
