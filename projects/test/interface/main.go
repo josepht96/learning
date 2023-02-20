4ca82ced35648933b5f6854ba4e993ecbe976162
josepht96 <joebthomas4@gmail.com>
2023-02-20 12:07:59 -0600

    add

---
+++ projects/test/interface/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type doctorStruct struct {
+	Name      string
+	MedSchool string
+}
+
+type lawyerStruct struct {
+	Name      string
+	LawSchool string
+}
+
+func (d *doctorStruct) PrintName() string {
+	fmt.Println(d.MedSchool)
+	return "kek"
+}
+
+func (l *lawyerStruct) PrintName() string {
+	fmt.Println(l.LawSchool)
+	return "kek"
+}
+
+type employeeInterface interface {
+	PrintName() string
+}
+
+func printNameInterface(employee employeeInterface) {
+	_ = employee.PrintName()
+}
+
+func main() {
+	doctor1 := &doctorStruct{
+		Name:      "Joe T",
+		MedSchool: "Tufts Med",
+	}
+	lawyer1 := &lawyerStruct{
+		Name:      "Joe T",
+		LawSchool: "Tufts Law",
+	}
+	printNameInterface(doctor1)
+	printNameInterface(lawyer1)
+}
