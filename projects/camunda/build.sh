6dfbc6fdabad5b8237976057d0b4012c49d69121
josepht96 <joebthomas4@gmail.com>
2023-04-28 14:31:11 -0500

    add

--- projects/camunda/build.sh
+++ projects/camunda/build.sh
@@ -1,2 +1,2 @@
-docker build -t josepht96/zeebe-worker-stress-test:v1 .
+docker build -t josepht96/zeebe-worker-stress-test:v1 --platform linux/amd64 .
 docker push josepht96/zeebe-worker-stress-test:v1
