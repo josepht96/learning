af3ddb655f2f5fd7710524bd0cc68b8286536d17
josepht96 <joebthomas4@gmail.com>
2024-01-02 11:23:06 -0600

    add

--- projects/test/jaeger/build.sh
+++ projects/test/jaeger/build.sh
@@ -1,4 +1,4 @@
-docker build -t josepht96/jaeger-test:latest .
-docker push josepht96/jaeger-test:latest
-kubectl delete -f deployment.yaml
-kubectl apply -f deployment.yaml
+docker build -t josepht96/jaeger-test:v0.0.1 .
+docker push josepht96/jaeger-test:v0.0.1
+kubectl delete -f deploy.yaml
+kubectl apply -f deploy.yaml
