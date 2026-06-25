8751c5820f7f337fc27572022c7bfe395e106881
josepht96 <joebthomas4@gmail.com>
2026-06-25 11:41:07 -0500

    add

--- debug/batch-job/patch.sh
+++ debug/batch-job/patch.sh
@@ -1,14 +1,23 @@
-curl -X PUT "http://localhost:9200/operate-batch-operation-1.0.0_/_mapping" -H 'Content-Type: application/json' -d '{
-  "properties": {
-    "errors": {
-      "properties": {
-        "message": {"type": "text"},
-        "partitionId": {"type": "integer"},
-        "type": {"type": "keyword"}
-      }
-    },
-    "operationsCompletedCount": {"type": "long"},
-    "operationsFailedCount": {"type": "long"},
-    "state": {"type": "keyword"}
-  }
-}'
+indices=(
+  "operate-batch-operation-1.0.0_"
+  # add other affected dated indices here
+)
+
+for idx in "${indices[@]}"; do
+  echo "=== Updating $idx ==="
+  curl -X PUT "http://localhost:9200/$idx/_mapping" -H 'Content-Type: application/json' -d '{
+    "properties": {
+      "errors": {
+        "properties": {
+          "message": {"type": "text"},
+          "partitionId": {"type": "integer"},
+          "type": {"type": "keyword"}
+        }
+      },
+      "operationsCompletedCount": {"type": "long"},
+      "operationsFailedCount": {"type": "long"},
+      "state": {"type": "keyword"}
+    }
+  }'
+  echo ""
+done
