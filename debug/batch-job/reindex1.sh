b8be543d46011e20594a5fefcd814fc8c5c6e79a
josepht96 <joebthomas4@gmail.com>
2026-06-25 13:22:30 -0500

    automatic commit

--- debug/batch-job/reindex1.sh
+++ debug/batch-job/reindex1.sh
@@ -1,11 +1,18 @@
+#!/bin/bash
+# Find all batch-operation indices with the old text-type id field
+old_indices=()
+
 for idx in $(curl -s "http://localhost:9200/_cat/indices/*batch-operation*?h=index"); do
-  echo "=== $idx ==="
-  curl -s "http://localhost:9200/$idx/_mapping" | python3 -c "
+  id_type=$(curl -s "http://localhost:9200/$idx/_mapping" | python3 -c "
 import sys, json
 d = json.load(sys.stdin)
 for index_name, body in d.items():
-    props = body['mappings']['properties']
-    id_type = props.get('id', {}).get('type')
-    print(f\"id field type: {id_type}\")
-"
+    print(body['mappings']['properties'].get('id', {}).get('type', ''))
+")
+  if [ "$id_type" == "text" ]; then
+    old_indices+=("$idx")
+  fi
 done
+
+echo "Found ${#old_indices[@]} indices to fix"
+printf '%s\n' "${old_indices[@]}"
