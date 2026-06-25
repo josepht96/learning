02d1ee8fcbd5b12a9167c619f77748439ed9dabe
josepht96 <joebthomas4@gmail.com>
2026-06-25 13:19:46 -0500

    automatic commit

--- debug/batch-job/reindex1.sh
+++ debug/batch-job/reindex1.sh
@@ -1,4 +1,11 @@
 for idx in $(curl -s "http://localhost:9200/_cat/indices/*batch-operation*?h=index"); do
   echo "=== $idx ==="
-  curl -s "http://localhost:9200/$idx/_mapping" | grep -A1 '"id"'
+  curl -s "http://localhost:9200/$idx/_mapping" | python3 -c "
+import sys, json
+d = json.load(sys.stdin)
+for index_name, body in d.items():
+    props = body['mappings']['properties']
+    id_type = props.get('id', {}).get('type')
+    print(f\"id field type: {id_type}\")
+"
 done
