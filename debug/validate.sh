788c78e8496579181cdc3684012e83f1abfa82d8
ad_app_joseph.thomas <joseph.thomas@fda.hhs.gov>
2026-06-25 10:27:23 -0500

    add

--- debug/validate.sh
+++ debug/validate.sh
@@ -1,4 +1,10 @@
-for idx in <index1> <index2> <index3>; do
+indices=(
+  operate-post-importer-queue-8.3.0_2024-09-20
+  operate-post-importer-queue-8.3.0_2024-09-23
+  operate-post-importer-queue-8.3.0_2024-09-22
+)
+
+for idx in "${indices[@]}"; do
   echo "=== $idx ==="
   curl -s "http://localhost:9200/$idx/_count"
   echo ""
