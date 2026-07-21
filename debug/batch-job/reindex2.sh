e0922acafb29da522abad74605b62061cf7b003c
ad_app_joseph.thomas <joseph.thomas@fda.hhs.gov>
2026-07-21 16:04:02 -0500

    fix: adding

--- debug/batch-job/reindex2.sh
+++ debug/batch-job/reindex2.sh
@@ -1,3 +1,19 @@
+old_indices=(
+operate-batch-operation-1.0.0_2023-10-11
+operate-batch-operation-1.0.0_2023-09-26
+operate-batch-operation-1.0.0_2023-09-27
+operate-batch-operation-1.0.0_2023-10-10
+operate-batch-operation-1.0.0_2023-09-28
+operate-batch-operation-1.0.0_2023-09-29
+operate-batch-operation-1.0.0_2023-09-12
+operate-batch-operation-1.0.0_2023-09-15
+operate-batch-operation-1.0.0_2023-09-19
+operate-batch-operation-1.0.0_2023-10-09
+operate-batch-operation-1.0.0_2023-10-06
+operate-batch-operation-1.0.0_2023-08-30
+operate-batch-operation-1.0.0_2023-08-31
+)
+
 for idx in "${old_indices[@]}"; do
   echo "=== Reindexing $idx ==="
 
